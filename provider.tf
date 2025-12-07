825bb27d440ec2d2347b812f80e27fa07421bd48
Sirin Ali <118505493+sirinali07@users.noreply.github.com>
2025-12-07 14:49:03 +0530

    Add Azure provider configuration in provider.tf

---
+++ provider.tf
@@ -0,0 +1,9 @@
+ provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+  # Connection to Azure
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+}
