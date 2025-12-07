680d8c579897f0916cae347ee935feed7b9c665f
Sirin Ali <118505493+sirinali07@users.noreply.github.com>
2025-12-07 15:06:21 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "RG" {
-  name     = "Azure_Tf_Resource-_Group"
+  name     = "Azure_Tf_Resource-_Group_0712"
   location = "East US"
   tags = {
     client = "Sirin"
